a83dbf442746340e5c691473f3eb9290b632bc04
wuzhiming <wuzhiming_cd@keruyun.com>
2017-08-29 16:47:36 +0800

    add

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(reuest):
-    return HttpResponse("Rango says hey there world!<br/><a href='/rango/about'>About</a>")
+def index(request):
+    context_dict = {'boldmessage':"I am bold font from the context"}
+    # return HttpResponse("Rango says hey there world!<br/><a href='/rango/about'>About</a>")
+    return render(request,'rango/index.html',context_dict)
 def about(reuest):
     return HttpResponse("Rango says here is the about page!<br/><a href='/rango'>Index</a>")
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include,url
 from django.contrib import admin
-
+from django.views.static import serve
+from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rango/', include('rango.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns.append(url(r'^media/(?P<path>.*)$',  serve, {"document_root": settings.MEDIA_ROOT}),)
