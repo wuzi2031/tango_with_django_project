3789d8a286324ddea7d1f95d603742eb7709730a
wzm <wuzhi_m@126.com>
2017-09-13 23:00:51 +0800

    add login

--- rango/urls.py
+++ rango/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [url(r'^$', views.index, name='index'),
                url(r'^add_category/$', views.add_category, name='add_category'),
                url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
                url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
-               url(r'^register/$', views.register, name='register'), ]
+               url(r'^register/$', views.register, name='register'),
+               url(r'^login/$', views.user_login, name='login'), ]
--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from rango.models import Category, Page, UserProfile
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 from rango.form import CategoryForm, PageForm, UserForm, UserProfileForm
 
 
@@ -100,3 +103,21 @@ def register(request):
     dict['profile_form'] = profile_form
     dict['registed'] = registed
     return render(request, 'rango/register.html', dict)
+
+
+def user_login(request):
+    if (request.method == 'POST'):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if (user):
+            if (user.is_active):
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/user_login.html', {})
