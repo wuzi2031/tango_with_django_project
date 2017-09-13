4382171adbba16fb1e613994bb05fb8997425b91
wuzhiming <wuzhiming_cd@keruyun.com>
2017-09-13 18:30:19 +0800

    add

--- rango/models.py
+++ rango/models.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
-from django.contrib.auth.models import User
 
 
 # Create your models here.
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include,url
+from django.conf import settings
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.static import serve
-from django.conf import settings
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rango/', include('rango.urls')),
 ]
 if settings.DEBUG:
-    urlpatterns.append(url(r'^media/(?P<path>.*)$',  serve, {"document_root": settings.MEDIA_ROOT}),)
+    urlpatterns.append(url(r'^media/(?P<path>.*)$', serve, {"document_root": settings.MEDIA_ROOT}), )
